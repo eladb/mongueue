329e263d64b8b291c5be5b3c39b53818cc195a58
Elad Ben-Israel <elad.benisrael@gmail.com>
2011-08-15 18:29:49 +0300

    remove dependency of mongodb

--- stress.cmd
+++
@@ -1,3 +0,0 @@
-start mongod --port 60000 --dbpath ./tempdb
-call node examples/stress.js
-taskkill /im mongod.exe
--- test.cmd
+++
@@ -1,3 +0,0 @@
-start mongod --port 60000 --dbpath ./tempdb
-call node node_modules/vows/bin/vows test/* --spec %1 %2 %3 %4 %5 %6
-taskkill /im mongod.exe
